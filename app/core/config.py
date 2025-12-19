11cc37fa4e8633a529459cd9d6b092b611883e8c
benf-le <banglebang2016@gmail.com>
2025-12-19 15:40:30 +0700

    change path file

--- app/core/config.py
+++ app/core/config.py
@@ -18,8 +18,8 @@ class Settings(BaseSettings):
     # Model paths
     BASE_DIR: Path = Path(__file__).resolve().parent.parent.parent
     WEIGHTS_DIR: Path = BASE_DIR / "weights"
-    DETECTOR_MODEL_PATH: Path = WEIGHTS_DIR / "Model_det_small"
-    RECOGNIZER_MODEL_PATH: Path = WEIGHTS_DIR / "Model_rec"
+    DETECTOR_MODEL_PATH: Path = WEIGHTS_DIR / "Model_det_small/"
+    RECOGNIZER_MODEL_PATH: Path = WEIGHTS_DIR / "Model_rec/"
     
     # Detection settings
     DETECTION_RESIZE_LONG: int = 960
